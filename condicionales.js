40d225492ef0ca7adcf083f35818fa92652f3e53
Melani Elizondo <melanimaria98@gmial.com>
2024-04-12 19:58:39 -0300

    guardando primeros cambios

---
+++ condicionales.js
@@ -0,0 +1,9 @@
+// For => Este ciclo tiene la declaracion de la variable, la condicion y el incremento en una sola linea
+for (let i = 0 ; i < 7; i++) {
+    console.log ("for"+ i)
+}
+
+let numeros = [10, 20, 30, 50, 80]
+for (let i = 0; i < numeros.length; i ++) {
+    console.log (numeros [i]);
+}
